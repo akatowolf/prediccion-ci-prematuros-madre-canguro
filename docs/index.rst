daab22ccf5496136e58ea91ee0bb50a5502c131c
akatowolf <akatowolf@hotmail.com>
2026-03-29 19:17:59 -0500

    Estructura proyecto grado

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. prediccion-ci-prematuros-madre-canguro documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+prediccion-ci-prematuros-madre-canguro documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='El proyecto surge de la Fundación Canguro, ONG colombiana líder en la investigación y difusión del Método Madre Canguro (MMC) a nivel global. Desde 1978, la Fundación encabeza un estudio de seguimiento longitudinal pionero que compara el MMC frente al cuidado en incubadora mediante ensayos controlados aleatorizados. Los resultados, sustentados en neuroimágenes y tests cognitivos, demuestran que el MMC favorece significativamente el desarrollo neurológico y cognitivo del prematuro hasta la adultez.',
+    author='Universidad de los Andes Grupo 13',
+    license='',
+)
